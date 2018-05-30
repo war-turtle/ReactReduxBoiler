79c3961a34ac5918e544d188c557fe0933c1a74d
war-turtle <kartikyadav71199@gmail.com>
2018-05-30 09:16:21 -0700

    devServer added

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
+const devMode = process.env.NODE_ENV !== 'production';
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
-    rules: [{ test: /\.js$/, use: 'babel-loader' }]
-  }
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+      {
+        test: /\.css$/,
+        use: [devMode ? 'style-loader' : MiniCssExtractPlugin, 'css-loader']
+        // use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ]
 };
